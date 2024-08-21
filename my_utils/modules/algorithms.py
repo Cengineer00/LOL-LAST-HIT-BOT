8a3ff5db64507996ea0621d25a85780fee816838
orhun-RAI <orhun.kose@radius.ai>
2024-08-21 15:31:26 +0300

    update object associator

--- my_utils/modules/algorithms.py
+++ my_utils/modules/algorithms.py
@@ -1,6 +1,7 @@
 from typing import Any
 from scipy.optimize import linear_sum_assignment
 from my_utils.data_structures.minion import Minion
+from my_utils.modules.utils.health_bar_utils import HealthBarUtils
 import numpy as np
 import sys
 
@@ -12,11 +13,15 @@ class ObjectAssociator:
 
     def __init__(self, intersection_threshold = 0.5):
         self.intersection_threshold = intersection_threshold
+        self.health_bar_utils = HealthBarUtils()
 
-    def __call__(self, minions, health_bars):
-        self.match_bars_to_minions(minions, health_bars)
+    def __call__(self, detection_result, img, minion_type = 'red'):
+        minions = detection_result[f'{minion_type}_minions']
+        health_bars = detection_result[f'{minion_type}_health_bars']
+        
+        return self.match_bars_to_minions(minions, health_bars, img)
 
-    def match_bars_to_minions(self, minions, health_bars):
+    def match_bars_to_minions(self, minions, health_bars, img):
 
         if len(minions) == 0 or len(health_bars) == 0: return list()
 
@@ -30,10 +35,12 @@ class ObjectAssociator:
         for hbar_idx, minion_idx in zip(row_ind, col_ind):
             if hbars_vs_minions_ious[hbar_idx, minion_idx] > self.intersection_threshold:
                 xyxy = health_bar_boxes[hbar_idx]
-                health_level = self.health_bar_utils.get_health_level(health_bar_img=img[int(xyxy[1]):int(xyxy[3]), int(xyxy[0]):int(xyxy[2])], bar_color='red')
+                if 'red' in minions[minion_idx][1]: health_level = self.health_bar_utils.get_health_level(health_bar_img=img[int(xyxy[1]):int(xyxy[3]), int(xyxy[0]):int(xyxy[2])], bar_color='red')
+                else: health_level = self.health_bar_utils.get_health_level(health_bar_img=img[int(xyxy[1]):int(xyxy[3]), int(xyxy[0]):int(xyxy[2])], bar_color='blue')
+
                 matched_minions.append(Minion(
                     minion_type=minions[minion_idx][1], 
-                    health_level=health_bars[hbar_idx][1], 
+                    health_level=health_level, 
                     bbox=minions[minion_idx][0],
                     confidence=minions[minion_idx][2],
                 ))
@@ -46,6 +53,24 @@ class ObjectAssociator:
         for minion in minions:
             label = f'{minion.minion_type} {minion.confidence:.2f}, health: {100*minion.health_level:.1f}%'
             annotator.box_label(minion.bbox, label, color=color)
+
+        # Stream results
+        return annotator.result()
+    
+    def annotate_minions2(self, img, minions: list[Minion], colors:list, line_thickness = 1, annotate_health_level=True, annotate_detections=False):
+        annotator = Annotator(img, line_width=line_thickness, font_size=15)
+
+        if annotate_detections:
+            for i, minion_box in enumerate(minions):
+                annotator.box_label(minion_box, color=colors[i])
+        elif annotate_health_level:
+            for i, minion in enumerate(minions):
+                label = f'{minion.minion_type}, health: {100*minion.health_level:.1f}%'
+                annotator.box_label(minion.bbox, label, color=colors[i])
+        else:
+            for i, minion in enumerate(minions):
+                label = f'{minion.minion_type}'
+                annotator.box_label(minion.bbox, label, color=colors[i])
 
         # Stream results
         return annotator.result()
